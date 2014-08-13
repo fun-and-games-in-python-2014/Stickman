76da72aef9ebd16dd2695d45ee605254864a2af2
Maxwell R <Maxwell@reality.com>
2014-08-13 13:16:39 -0700

    hi

--- stickmangame7.py
+++ stickmangame7.py
@@ -5,7 +5,7 @@ import time
 class Game:
     def __init__(self):
         self.tk = Tk()
-        self.tk.title("Mr. Stick Man Races for the Exit")
+        self.tk.title("Agent Stick Man Races for the Exit")
         self.tk.resizable(0, 0)
         self.tk.wm_attributes("-topmost", 1)
         self.canvas = Canvas(self.tk, width=500, height=500, highlightthickness=0)
